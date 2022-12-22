e5f7dde331b341c84ca634d6d8f317bfed074805
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-22 23:41:35 +0400

    MenuUtil

--- src/util/MenuUtil.java
+++ src/util/MenuUtil.java
@@ -4,6 +4,9 @@ public class MenuUtil {
 
     public static void showMenu() {
         Menu[] menus = Menu.values();
+        for (int i=0;i<menus.length;i++){
+            System.out.println(menus[i]);
+        }
     }
 
 }
