8336b42683d9aa3b522709180bcadd483391bb88
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-24 22:10:35 +0400

    MenuLoginService

--- src/service/menu/MenuLoginService.java
+++ src/service/menu/MenuLoginService.java
@@ -1,7 +1,30 @@
 package service.menu;
 
+import bean.Config;
 import service.menu.inter.MenuLoginServiceInter;
 
+import java.util.Scanner;
+
 public class MenuLoginService implements MenuLoginServiceInter {
+
+    @Override
+    public void process() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Username:");
+        String username = scanner.nextLine();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Password:");
+        String password = scanner2.nextLine();
+
+        if (!(username.equals("user") && password.equals("11111"))) {
+            throw new IllegalArgumentException("username or password invalid");
+        }
+        Config.setLoggedIn(true);
+
+
+    }
+
 
 }
