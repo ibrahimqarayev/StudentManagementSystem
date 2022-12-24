e09e62156e7709ccaa64c3c330c7a2f2dadd10d7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-24 22:10:54 +0400

    MenuRegisterService

--- src/service/menu/MenuRegisterService.java
+++ src/service/menu/MenuRegisterService.java
@@ -5,6 +5,9 @@ import service.menu.inter.MenuRegisterServiceInter;
 public class MenuRegisterService implements MenuRegisterServiceInter {
     @Override
     public void process() {
+
         System.out.println("Register");
+
+
     }
 }
