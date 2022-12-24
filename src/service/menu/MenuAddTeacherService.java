1b19cf1ba4b5fc7d7ca153a119c3239333757c60
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-24 22:10:18 +0400

    MenuAddTeacherService

---
+++ src/service/menu/MenuAddTeacherService.java
@@ -0,0 +1,30 @@
+package service.menu;
+
+import bean.Config;
+import bean.Teacher;
+import service.menu.inter.MenuAddTeacherServiceInter;
+
+import java.util.Scanner;
+
+public class MenuAddTeacherService implements MenuAddTeacherServiceInter {
+
+    @Override
+    public void process() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name:");
+        String name = scanner.nextLine();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Surname:");
+        String surname = scanner2.nextLine();
+
+        Teacher teacher = new Teacher();
+        teacher.setName(name);
+        teacher.setSurname(surname);
+
+        Config.instance().appendTeacher(teacher);
+        System.out.println("Teacher added !");
+
+    }
+}
