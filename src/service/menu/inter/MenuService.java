120bc94fbc04cf8f841aefc457a5f03ae665244d
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-26 22:28:41 +0400

    MenuService

--- src/service/menu/inter/MenuService.java
+++ src/service/menu/inter/MenuService.java
@@ -1,9 +1,12 @@
 package service.menu.inter;
 
+import bean.Config;
+
 public interface MenuService {
 
     default void process() {
         processAbstract();
+        Config.save();
     }
 
     public abstract void processAbstract();
