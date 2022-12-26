bbff2b488bad3c6ff5a4b8bdd833ef24ba85c8d4
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-26 22:32:10 +0400

    MenuShowTeacherService

--- src/service/menu/MenuShowTeacherService.java
+++ src/service/menu/MenuShowTeacherService.java
@@ -7,7 +7,7 @@ import service.menu.inter.MenuShowTeacherServiceInter;
 public class MenuShowTeacherService implements MenuShowTeacherServiceInter {
 
     @Override
-    public void process() {
+    public void processAbstract() {
         Teacher[] teachers = Config.instance().getTeachers();
         for (int i = 0; i < teachers.length; i++) {
             System.out.println(teachers[i]);
