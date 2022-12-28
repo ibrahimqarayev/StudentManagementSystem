ba132dee48edaedcbe8b19778c5da3bad52c2a97
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-28 11:18:14 +0400

    MenuShowTeacherService

--- src/service/menu/MenuShowTeacherService.java
+++ src/service/menu/MenuShowTeacherService.java
@@ -2,12 +2,12 @@ package service.menu;
 
 import bean.Config;
 import bean.Teacher;
-import service.menu.inter.MenuShowTeacherServiceInter;
+import service.inter.menu.MenuShowTeacherServiceInter;
 
 public class MenuShowTeacherService implements MenuShowTeacherServiceInter {
 
     @Override
-    public void processAbstract() {
+    public void processLogic() {
         Teacher[] teachers = Config.instance().getTeachers();
         for (int i = 0; i < teachers.length; i++) {
             System.out.println(teachers[i]);
