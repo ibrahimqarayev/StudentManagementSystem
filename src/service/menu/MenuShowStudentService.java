eeca818688af2c3e61949b2cedcfd2e30cc0c4e8
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-28 11:18:37 +0400

    MenuShowStudentService

--- src/service/menu/MenuShowStudentService.java
+++ src/service/menu/MenuShowStudentService.java
@@ -2,12 +2,12 @@ package service.menu;
 
 import bean.Config;
 import bean.Student;
-import service.menu.inter.MenuShowStudentServiceInter;
+import service.inter.menu.MenuShowStudentServiceInter;
 
 public class MenuShowStudentService implements MenuShowStudentServiceInter {
 
     @Override
-    public void processAbstract() {
+    public void processLogic() {
         Student[] students = Config.instance().getStudents();
         for (int i = 0; i < students.length; i++) {
             System.out.println(students[i]);
